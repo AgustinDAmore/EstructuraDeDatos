be65602ca799269d34d350a3757d5d61bbc7b3ba
AgustinDamore <agustindamore50@gmail.com>
2022-10-16 16:29:54 -0300

    modified:   Pila.py

--- Pila.py
+++ Pila.py
@@ -1,23 +1,37 @@
 class Pila:
-    def __init__(self):
+    def __init__(self) -> None: 
         self.items = []
+        self.tamano = 0
+        
+    def tamano(self) -> int:
+        return self.tamano
 
-    def estaVacia(self):
-        return self.items == []
-
-    def apilar(self, item):
+    def apilar(self, item) -> None:
         self.items.append(item)
+        self.tamano += 1
 
-    def desapilar(self):
+    def desapilar(self) -> object:
         return self.items.pop()
+        self.tamano -= 1
 
-    def tamano(self):
-        return len(self.items)
-
-    def cima(self):
+    def cima(self) -> object:
         return self.items[len(self.items)-1]
     
-    def mostrar(self):
-        return self.items
+    def Vacia(self) -> bool:
+        return self.tamano == 0
 
-        
+    def mostrar(self) -> list:
+        return self.items
+    
+    def vaciar(self):
+        self.items = []
+        self.tamano = 0
+
+    def __repr__(self):
+        return str(self.items)
+    
+    def __iter__(self):
+        return iter(self.items)
+    
+    def __len__(self):
+        return self.tamano()
